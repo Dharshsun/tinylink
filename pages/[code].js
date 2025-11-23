14d201fe175f38774a5dad4754746a8f6858df35
Dharshsun. S <62539407+Dharshsun@users.noreply.github.com>
2025-11-23 10:22:51 +0530

    Update [code].js

--- pages/[code].js
+++ pages/[code].js
@@ -1,4 +1,4 @@
-import pool from '../../../lib/db.cjs';
+import pool from '../lib/db.cjs';
 const CODE_REGEX = /^[A-Za-z0-9]{6,8}$/;
 function randomCode(len = 6){
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
